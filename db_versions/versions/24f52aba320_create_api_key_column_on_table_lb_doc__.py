ea00b1f93c34263eb2335c7ce88e7b454980a611
Alessandro Landim <landim@Alessandros-iMac.local>
2020-11-01 21:26:55 -0300

    lbgenerator updates to install on Docker

--- db_versions/versions/24f52aba320_create_api_key_column_on_table_lb_doc__.py
+++ db_versions/versions/24f52aba320_create_api_key_column_on_table_lb_doc__.py
@@ -18,23 +18,23 @@ down_revision = '541f8046f693'
 
 def upgrade():
     op.execute('ALTER TABLE lb_doc__user ADD COLUMN api_key character varying(200);')
-    op.execute(' TABLE lb_doc__user DROP COLUMN status_user;')
+    #op.execute(' TABLE lb_doc__user DROP COLUMN status_user;')
 
 
     #For old version
-    op.execute('ALTER TABLE lb_doc__user DROP COLUMN id_user;')
-    op.execute('ALTER TABLE lb_doc__user DROP COLUMN email_user;')
-    op.execute('ALTER TABLE lb_doc__user DROP COLUMN name_base;')
-    op.execute('ALTER TABLE lb_doc__user DROP COLUMN creation_date_user;')
-    op.execute('ALTER TABLE lb_doc__user DROP COLUMN status_user;')
-    op.execute('ALTER TABLE lb_doc__user ADD COLUMN id_user character varying(200);')
+    #op.execute('ALTER TABLE lb_doc__user DROP COLUMN id_user;')
+    #op.execute('ALTER TABLE lb_doc__user DROP COLUMN email_user;')
+    #op.execute('ALTER TABLE lb_doc__user DROP COLUMN name_base;')
+    #op.execute('ALTER TABLE lb_doc__user DROP COLUMN creation_date_user;')
+    #op.execute('ALTER TABLE lb_doc__user DROP COLUMN status_user;')
+    #op.execute('ALTER TABLE lb_doc__user ADD COLUMN id_user character varying(200);')
     
     account = table('lb_base',
-      	column('name', String)
+        column('name', String)
     )
     op.execute(
-     	account.delete().\
-         	where(account.c.name==op.inline_literal('_user'))
+        account.delete().\
+            where(account.c.name==op.inline_literal('_user'))
     )
     pass
 
